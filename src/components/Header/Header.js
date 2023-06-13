4dd41a774818d7dc9bbea43fb2bae4761a470fef
lavanmetta <lavanmetta111@gmail.com>
2023-06-13 23:17:40 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import navlogo from "../../Assets/images/navlogo.png";
 import "./Header.css";
+
 const Header = () => {
+  const [name, setName] = useState("nav-links1");
+  const navLinksRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (navLinksRef.current && !navLinksRef.current.contains(event.target)) {
+        setName("nav-links1");
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+  const onClickBurger = () => {
+    setName("nav-links2");
+  };
+
   return (
     <div className="header-container">
       <div className="nav-logo">
         <img src={navlogo} alt="" />
         <h2>TasteMolecule</h2>
       </div>
-      <div className="nav-links">
+
+      <div className="burger">
+        <i className="ri-menu-line" onClick={onClickBurger}></i>
+      </div>
+      <div ref={navLinksRef} className={`nav-links ${name}`}>
         <Link to="/">
           <i className="ri-gift-2-line"></i>Offers
         </Link>
